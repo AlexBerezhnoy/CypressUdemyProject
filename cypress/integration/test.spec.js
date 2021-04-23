b11bb1d79c8a7350424cf2e73e4d16921ace85a1
Alex Berezhnoy <dinnylol221@gmail.com>
2021-04-23 21:06:05 +0300

    requests

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,16 +1,22 @@
 ///<reference  types="Cypress" />
 
-import {mobileReplanishment} from "../support/pages/mobileReplenishment"
-import {transfers} from "../support/pages/transfers"
-import {basePage} from "../support/pages/basePage"
-import * as CardConstans  from '../support/constants'
+import {
+    mobileReplanishment
+} from "../support/pages/mobileReplenishment"
+import {
+    transfers
+} from "../support/pages/transfers"
+import {
+    basePage
+} from "../support/pages/basePage"
+import * as CardConstans from '../support/constants'
 
-describe('Privat24 tests', ()=>{
-    it('Replanishment of Ukraine mobile phone number', ()=>{
+describe('Privat24 tests', () => {
+    it('Replanishment of Ukraine mobile phone number', () => {
         basePage.open('https://next.privat24.ua/mobile?lang=en')
         mobileReplanishment.typePhoneNumber('680310775')
         basePage.typeAmount('1')
-        basePage.typeDebitCardData(CardConstans.card.number, CardConstans.card.expireDate , CardConstans.card.cvv)
+        basePage.typeDebitCardData(CardConstans.card.number, CardConstans.card.expireDate, CardConstans.card.cvv)
         cy.wait(3000)
         basePage.submitPayment()
         mobileReplanishment.checkDebitCard('4552 **** **** 8217')
@@ -20,10 +26,10 @@ describe('Privat24 tests', ()=>{
         mobileReplanishment.checkCommissionCurrency('UAH')
     })
 
-    it('Many transfer beetwen foreigh cards ', ()=>{
+    it('Many transfer beetwen foreigh cards ', () => {
         basePage.open('https://next.privat24.ua/money-transfer/card?lang=en')
         //Type data
-        basePage.typeDebitCardData(CardConstans.card.number, CardConstans.card.expireDate , CardConstans.card.cvv)
+        basePage.typeDebitCardData(CardConstans.card.number, CardConstans.card.expireDate, CardConstans.card.cvv)
         transfers.typeDebitNameAndSurname('Shayne', 'McConnel')
         transfers.typeReceiverCard('5309233034765085')
         transfers.typeReceiverNameAndSurname('Juliana', 'Janssen')
@@ -31,12 +37,49 @@ describe('Privat24 tests', ()=>{
         transfers.typeComment('Cypress Test')
         cy.wait(3000)
         basePage.submitPayment()
-        
+
         //Checks
         transfers.checkPayerAndReceiverCard('* 8217', '* 5085')
         transfers.checkPayerAmountAndTotal('300 UAH', '388.63')
         transfers.checkCommission('88.63 UAH')
         transfers.checkTotalCurrency('UAH')
         transfers.checkComment('Cypress Test')
+    })
+
+    it('Example sending the Get request ', () => {
+        cy.request('https://next.privat24.ua')
+            .then((response) => {
+                console.log(response);
+            })
+
+    })
+
+    it.only('Example sending the POST request ', () => {
+
+        const requestBody = {
+            "action": "add",
+            "phone": "+380680310775",
+            "amount": 50,
+            "currency": "UAH",
+            "cardCvv": "111",
+            "card": "4552331448138217",
+            "cardExp": "0524",
+            "operator": "Kyivstar Ukraine",
+            "operatorId": "602",
+            "xref": "0b8ff67fcc3258161dad2cb1007a5d53",
+            "_": 1619200159450
+        }
+
+        const headersData = {cookie: "_ga=GA1.2.1758838676.1618080785; _gid=GA1.2.1020219545.1619199426; pubkey=317014d488b93a3ed119a246d82ec7fb; lfp=4/10/2021, 9:53:16 PM; pa=1619199438004.22070.10417836353285082next.privat24.ua0.9738190003556719+1; fp=11; _gat_gtag_UA_29683426_11=1"}
+        cy.request({
+                method: "POST",
+                url: "https://next.privat24.ua/api/p24/pub/mobipay",
+                body: requestBody,
+                headers: headersData
+            })
+            .then((response) => {
+                console.log(response.body);
+            })
+
     })
 })
